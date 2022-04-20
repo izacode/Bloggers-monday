659c5a24d298b270c86b460315869f3d8bc69450
unknown <izaelec@gmail.com>
2022-04-19 21:26:43 -0400

    modified update post response without status 204

--- src/routes/posts-routes.ts
+++ src/routes/posts-routes.ts
@@ -73,7 +73,7 @@ postsRouter.put(
       
       res.sendStatus(400)
     }else{
-      isUpdated ? res.status(204).send(isUpdated) : res.sendStatus(404);
+      isUpdated ? res.send(isUpdated) : res.sendStatus(404);
     }
     
   }
